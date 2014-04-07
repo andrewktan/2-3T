64acb9fd43d402810cfaaac0950ac5d976d551e1
Andrew Tan <andrewtsoc@gmail.com>
2014-04-07 11:40:29 -0400

    zero indexed everything

--- static/js/grid.js
+++ static/js/grid.js
@@ -1,6 +1,6 @@
 function Grid(terminal) {
     this.cells = new Array();
-    for (var i=1; i<=9; i++) {
+    for (var i=0; i<9; i++) {
         this.cells[i] = new Cell(i, 
             (terminal)? null : new Grid(true),
             terminal);
